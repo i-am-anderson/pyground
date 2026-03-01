454cb700fd2779d47d36aad756cec2567e98916e
AndersonSantosAraujo <anderson.dos.santos.araujo@hotmail.com>
2026-03-01 13:45:01 -0300

    feat: added resize panels

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import "./App.css";
+import { Panel, Group, Separator } from "react-resizable-panels";
 import CodeBlock from "./components/CodeBlock";
 import FilesTree from "./components/FilesTree";
 import Terminal from "./components/Terminal";
 
+// Um componente simples para a barra de arraste
+const ResizeHandle = ({ className = "" }) => (
+  <Separator
+    className={`w-1 bg-slate-800 hover:bg-blue-500 transition-colors ${className}`}
+  />
+);
+
+const ResizeHandleHorizontal = () => (
+  <Separator
+    className="h-1 bg-slate-800 hover:bg-blue-500 transition-colors"
+  />
+);
+
 function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <div style={{ width: "20%", height: "100%" }}>
-        <FilesTree />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "80%",
-          height: "100%",
-        }}
-      >
-        <div style={{ height: "65%" }}>
-          <CodeBlock />
-        </div>
-        <div style={{ height: "35%" }}>
-          <Terminal />
-        </div>
-      </div>
+    <div style={{ width: "100vw", height: "100vh", background: "#0f172a" }}>
+      {/* Grupo Principal: Horizontal (FilesTree vs Resto) */}
+      <Group orientation="horizontal">
+        
+        {/* Lado Esquerdo: FilesTree */}
+        <Panel defaultSize={20} minSize={10}>
+          <FilesTree />
+        </Panel>
+
+        <ResizeHandle />
+
+        {/* Lado Direito: Grupo Vertical (Code + Terminal) */}
+        <Panel defaultSize={80}>
+          <Group orientation="vertical">
+            
+            <Panel defaultSize={65} minSize={20}>
+              <CodeBlock />
+            </Panel>
+
+            <ResizeHandleHorizontal />
+
+            <Panel defaultSize={35} minSize={10}>
+              <Terminal />
+            </Panel>
+
+          </Group>
+        </Panel>
+
+      </Group>
     </div>
   );
 }
